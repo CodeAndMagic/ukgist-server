3dcb2cf9973c792f101c2351f07bd9493008b39c
Cristian Vrabie <cristian.vrabie@gmail.com>
2013-04-24 15:58:10 +0100

    Fixed sh script for police kml import

--- tools/police_kml_import.sh
+++ tools/police_kml_import.sh
@@ -1,3 +1,16 @@
 #!/bin/sh
+function concatenate_args
+{
+    string="run-main com.codeandmagic.ukgist.tools.PoliceKmlImport"
+    for a in "$@" # Loop over arguments
+    do
+        string+=" $a"
+    done
+    echo "$string"
+}
+
+# Usage:
+args="$(concatenate_args "$@")"
+echo $args
 cd ..
-sbt 'run-main com.codeandmagic.ukgist.tools.PoliceKmlImport "$@"'
+sbt "$args"
